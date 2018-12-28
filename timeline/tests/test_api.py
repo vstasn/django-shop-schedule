a030276f6e079d32764a02b1f59fbfb4af60556f
vstasn <vstasn@gmail.com>
2018-12-28 20:17:45 +0500

    Add tests: views and apis

---
+++ timeline/tests/test_api.py
@@ -0,0 +1,272 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from rest_framework.authtoken.models import Token
+from timeline.models import Shop, Daysoff, Entry
+from timeline.views import ShopDetail
+from freezegun import freeze_time
+from django.test import override_settings
+import json
+
+User = get_user_model()
+
+
+class BaseAPITest(APITestCase):
+    client = APIClient()
+
+    def _create_user(self):
+        user = User(username='user1')
+        user.set_password('pass1')
+        user.save()
+        return user
+
+    def _login_user(self, user):
+        token = Token.objects.create(user=user)
+        self.client.force_authenticate(user=user, token=token.key)
+
+    def _create_shop(self, owner):
+        return Shop.objects.create(owner=owner)
+
+    def _set_shop_view(self):
+        view = ShopDetail()
+        view.basename = 'shop'
+        view.request = None
+        return view
+
+
+class UserAPILoginTest(BaseAPITest):
+
+    def setUp(self):
+        self.user = self._create_user()
+
+    def test_user_login_and_create_token(self):
+        response = self.client.post(
+            reverse('login'),
+            {'username': 'user1', 'password': 'pass1'}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+
+class UserAPITest(BaseAPITest):
+
+    def test_user_register_valid(self):
+        response = self.client.post(
+            reverse('register'),
+            {'username': 'test', 'password': 'test'}
+        )
+
+        user = User.objects.first()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(user.username, 'test')
+
+    def test_user_register_invalid_empty_password(self):
+        response = self.client.post(
+            reverse('register'),
+            {'username': 'test'}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ShopAPITest(BaseAPITest):
+
+    def setUp(self):
+        self.user = self._create_user()
+        self._login_user(self.user)
+        self.view = self._set_shop_view()
+
+    def test_shop_can_save_POST_request(self):
+        self.client.post(
+            self.view.reverse_action('list'),
+            {'title': 'shop1'},
+        )
+
+        self.assertEqual(Shop.objects.count(), 1)
+        new_shop = Shop.objects.first()
+        self.assertEqual('shop1', new_shop.title)
+
+    def test_shop_trying_to_send_another_owner(self):
+        fraud_user = User.objects.create(username='test1')
+        self.client.post(
+            self.view.reverse_action('list'),
+            {'title': 'shop1', 'owner': fraud_user},
+        )
+
+        new_shop = Shop.objects.first()
+        self.assertNotEqual(new_shop.owner, fraud_user)
+
+    def test_shop_trying_to_update_foreign_shop(self):
+        foreign_user = User.objects.create(username='test1')
+        foreign_shop = Shop.objects.create(title='test', owner=foreign_user)
+
+        response = self.client.put(
+            self.view.reverse_action('detail', args=[foreign_shop.pk]),
+            {'title': 'shop1'}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertNotEqual(foreign_shop.owner, self.user)
+
+    def test_shop_owner_is_saved_if_user_is_authenticated(self):
+        response = self.client.post(
+            self.view.reverse_action('list'),
+            {'title': 'shop1'}
+        )
+
+        shop = Shop.objects.first()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.user, shop.owner)
+
+    def test_shop_trying_create_without_title(self):
+        response = self.client.post(
+            self.view.reverse_action('list'),
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_shop_trying_create_if_send_primary_key(self):
+        response = self.client.post(
+            self.view.reverse_action('list'),
+            {'title': 'shop1', 'id': 10}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertNotEqual(response.data['id'], 10)
+
+    def test_shop_can_close_if_user_is_authenticated(self):
+        shop = self._create_shop(self.user)
+        response = self.client.post(
+            self.view.reverse_action('close', [shop.pk]),
+        )
+        Daysoff.objects.filter(shop=shop).first()
+
+        self.assertTrue(shop.is_dayoff())
+        self.assertEqual(shop.pk, response.data['shop'])
+
+    def test_shop_trying_close_with_foreing_shop(self):
+        shop = self._create_shop(self.user)
+        shop_foreign = Shop.objects.create(owner=self.user)
+        response = self.client.post(
+            self.view.reverse_action('close', [shop.pk]),
+            {'shop': shop_foreign}
+        )
+
+        self.assertTrue(shop.is_dayoff())
+        self.assertEqual(shop.pk, response.data['shop'])
+
+    def test_shop_trying_close_with_invalid_params(self):
+        shop = self._create_shop(self.user)
+        response = self.client.post(
+            self.view.reverse_action('close', [shop.pk]),
+            {'from_date': 'fsffsfdsdf'}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_shop_trying_close_with_invalid_date_params(self):
+        shop = self._create_shop(self.user)
+        response = self.client.post(
+            self.view.reverse_action('close', [shop.pk]),
+            {'from_date': '2018-05-05', 'to_date': '2018-05-04'}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_shop_can_update_if_user_is_authenticated(self):
+        shop = self._create_shop(self.user)
+        self.client.post(
+            self.view.reverse_action('update-schedule', [shop.pk]),
+            {
+                'day_of_week': 0,
+                'from_time': '11.00',
+                'to_time': '20.00',
+                'breaks': json.dumps([{"from_time": "11.30", "to_time": "12.30"}])
+            }
+        )
+
+        count = Entry.objects.filter(shop=shop, day_of_week=0).count()
+
+        self.assertEqual(count, 2)
+
+    def test_shop_can_update_if_user_is_authenticated_without_breaks(self):
+        shop = self._create_shop(self.user)
+        self.client.post(
+            self.view.reverse_action('update-schedule', [shop.pk]),
+            {
+                'day_of_week': 0,
+                'from_time': '11.00',
+                'to_time': '20.00',
+            }
+        )
+
+        count = Entry.objects.filter(shop=shop, day_of_week=0).count()
+        self.assertEqual(count, 1)
+
+
+class ApiPermissionsTest(BaseAPITest):
+
+    def setUp(self):
+        self.view = self._set_shop_view()
+
+    def test_cant_create_shop_without_token(self):
+        response = self.client.post(
+            self.view.reverse_action('list'),
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_cant_update_shop_without_token(self):
+        response = self.client.post(
+            self.view.reverse_action('update-schedule', args=[1]),
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_cant_close_shop_without_token(self):
+        response = self.client.post(
+            self.view.reverse_action('close', args=[1]),
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+@override_settings(TIME_ZONE='UTC')
+class ShopAPIPublicTest(BaseAPITest):
+
+    def setUp(self):
+        user = self._create_user()
+        self.shop = self._create_shop(user)
+        self.view = self._set_shop_view()
+
+    @freeze_time('2018-12-20 08:00:00')
+    def test_can_check_shop_is_working(self):
+        response = self.client.post(
+            self.view.reverse_action('is-working', args=[self.shop.pk])
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('is_working', response.data)
+        self.assertTrue(response.data['is_working'])
+
+    @freeze_time('2018-12-20 03:00:00')
+    def test_can_check_shop_is_working_at_night(self):
+        response = self.client.post(
+            self.view.reverse_action('is-working', args=[self.shop.pk])
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('is_working', response.data)
+        self.assertFalse(response.data['is_working'])
+
+    def test_testshop_try_to_get_schedule(self):
+        url = self.view.reverse_action('schedule', args=[self.shop.pk])
+        response = self.client.post(
+            url
+        )
+        self.assertIn('working_hours', response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
---
+++ timeline/tests/test_models.py
@@ -0,0 +1,200 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.contrib import auth
+from timeline.models import Shop, Entry, Daysoff
+from freezegun import freeze_time
+import datetime
+
+User = auth.get_user_model()
+
+
+class UserModelTest(TestCase):
+    '''test user model'''
+
+    def test_user_is_valid_with_username_and_password(self):
+        '''test: validate user data'''
+        user = User(username='user1', password='11234')
+        user.full_clean()
+
+    def test_user_is_invalid_with_username(self):
+        '''test: validate user data without password'''
+        user = User(username='user1')
+        with self.assertRaises(ValidationError):
+            user.full_clean()
+
+
+class ShopModelTest(TestCase):
+    '''test shop model'''
+
+    def test_default_title(self):
+        shop = Shop()
+        self.assertEqual(shop.title, '')
+
+    def test_shop_is_related_to_user(self):
+        user_ = User.objects.create()
+        shop = Shop()
+        shop.title = 'shop1'
+        shop.owner = user_
+        shop.save()
+        self.assertIn(shop, user_.shop_set.all())
+
+    def test_shop_owner_is_not_optional(self):
+        '''test: should raise'''
+        with self.assertRaises(ValidationError):
+            Shop().full_clean()
+
+    def test_shop_can_have_owners(self):
+        '''test: should not raise'''
+        Shop(owner=User())
+
+    def test_shop_filter_is_closed_with_daysoff(self):
+        '''test: check filter, should raise'''
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+        # add days off from now
+        Daysoff.objects.create(shop=shop, from_date='2017-01-01', to_date='2017-01-02')
+        self.assertFalse(
+            Shop.objects.get(id=shop.pk).is_dayoff()
+        )
+
+    def test_shop_filter_is_closed_without_daysoff(self):
+        '''test: check filter, should raise'''
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+        self.assertFalse(
+            Shop.objects.get(id=shop.pk).is_dayoff()
+        )
+
+    def test_shop_filter_is_closed_with_daysoff_valid(self):
+        '''test: check filter, should not raise'''
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+        # close shop from now
+        Daysoff.objects.create(shop=shop)
+        is_dayoff = Shop.objects.get(id=shop.pk).is_dayoff()
+
+        self.assertTrue(is_dayoff)
+
+    def test_shop_default_schedule(self):
+        '''test: exists default schedule'''
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+        count = Entry.objects.filter(shop=shop).count()
+
+        self.assertGreater(count, 0)
+
+    def test_shop_is_working(self):
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+        Entry.objects.filter(shop=shop).count()
+        # close shop
+        Daysoff.objects.create(shop=shop)
+
+        self.assertFalse(shop.is_working())
+
+    def test_shop_is_working_at_night(self):
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+        with freeze_time('2018-12-20 03:01:00'):
+            self.assertFalse(shop.is_working())
+
+    def test_shop_find_working_hours(self):
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+
+        with freeze_time('2018-12-20 08:00:00'):
+            self.assertTrue(shop.by_working_time())
+
+    def test_shop_find_working_hours_at_break(self):
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+
+        with freeze_time('2018-12-20 11:30:00'):
+            self.assertFalse(shop.by_working_time())
+
+    def test_shop_find_working_hours_at_noon(self):
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+
+        with freeze_time('2018-12-20 00:00:00'):
+            self.assertTrue(shop.by_working_time())
+
+    def test_shop_find_working_hours_at_end_of_the_day(self):
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+
+        with freeze_time('2018-12-20 23:59:00'):
+            self.assertTrue(shop.by_working_time())
+
+
+class EntryModelTest(TestCase):
+    '''test entry model'''
+
+    def test_create_entry_without_shop(self):
+        '''test: should raise exceptions'''
+        entry = Entry()
+        with self.assertRaises(ValidationError):
+            entry.full_clean()
+
+    def test_create_entry_with_shop(self):
+        '''test: '''
+        user = User.objects.create()
+        shop = Shop.objects.create(owner=user)
+        Entry.objects.create(
+            shop=shop,
+            day_of_week=1,
+            from_time=int('01100'),
+            to_time=int('02100'),
+        )
+        entry = Entry.objects.first()
+        self.assertEqual(entry.shop, shop)
+
+
+class DaysoffModelTest(TestCase):
+    '''test daysoff model'''
+
+    def test_create_daysoff_without_shop(self):
+        '''test: should not raise exception'''
+        daysoff_ = Daysoff()
+        daysoff_.full_clean()
+
+    def test_create_daysoff_with_invalid_date(self):
+        '''test: trying to create daysoff with invalid date'''
+        daysoff_ = Daysoff()
+        daysoff_.from_date = '3903203'
+        with self.assertRaises(ValidationError):
+            daysoff_.full_clean()
+
+    def test_create_daysoff_with_valid_from_date(self):
+        '''test: set current date to from date (not default)'''
+        current = datetime.datetime.now().date()
+        daysoff_ = Daysoff.objects.create(from_date=current)
+
+        self.assertEqual(current, daysoff_.from_date)
+
+    def test_create_daysoff_with_default_from_date(self):
+        '''test: check if set default date'''
+        current = datetime.datetime.now().date()
+        daysoff_ = Daysoff.objects.create()
+
+        self.assertEqual(current, daysoff_.from_date)
+
+    def test_filter_is_closed_return_true(self):
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+
+        Daysoff.objects.create(shop=shop)
+
+        self.assertTrue(
+            Daysoff.objects.is_closed().exists()
+        )
+
+    def test_filter_is_closed_return_false(self):
+        user_ = User.objects.create()
+        shop = Shop.objects.create(owner=user_)
+
+        Daysoff.objects.create(shop=shop, from_date='2017-01-02', to_date='2017-02-02')
+
+        self.assertFalse(
+            Daysoff.objects.is_closed().exists()
+        )
